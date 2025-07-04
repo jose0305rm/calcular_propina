f8ce4fc52eab8fec4fc2dd0e2884b8a7ed5dd335
jose0305rm <81779667+jose0305rm@users.noreply.github.com>
2025-07-04 15:37:52 -0600

    se inicio el mostrar los datos de cada item para su manipulacion para generar la factura

--- src/components/Menuitems.tsx
+++ src/components/Menuitems.tsx
@@ -3,14 +3,14 @@ import type { MenuItem } from "../types";
 // Nuestros props
 type MenuItemProps = { 
   item: MenuItem, //inidicarle que espera un valor de tipo MenuItem
-  addItem: () => void //indicarle que este props espera un arrowfunction
+  addItem: (item: MenuItem) => void //indicarle que este props espera un item con la estructura MenuItem
 };
 
 export default function Menuitems({ item, addItem }: MenuItemProps) { //pasamos los props 
   return (
     <>
       <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
-        onClick={() => addItem()}> {/* se usa un arrow funcition para evitar que la funcion se ejecute apenas se llame */}
+        onClick={() => addItem(item)}> {/* se usa un arrow function para evitar que la funcion se ejecute apenas se llame */}
         <p>{item.name}</p> 
         <p className="font-bold">${item.price}</p>
         
